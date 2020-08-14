6e27d6151598ce3c308afac855267c699e03540c
Yuan Yang <yayuan@microsoft.com>
2020-08-14 11:16:12 +0800

    tmp

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ version = '0.4.3'
 
 # Dnspython is two different packages depending on python version
 if sys.version_info[0] == 2:
-    dns = 'dnspython'
+    dns = 'dnspython==1.16.0'
 else:
     dns = 'dnspython3'
 
